8f46d93de4ab5f95c108a5f20dc70791b5cba978
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2021-01-10 11:58:03 +0000

    Skeleton files and utils

---
+++ controller.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import figure, cm
+
+# import utils
+
+
+class Controller:
+    def __init__(self, verbose: bool = False):
+        self.verbose: bool = verbose
+
+        # hyperparameters
+
+    def run(self):
+        i: int = 0
+        pass
